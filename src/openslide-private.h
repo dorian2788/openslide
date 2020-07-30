61c7eb059a14bd1761788bc5273a8823f3fb5691
Nico-Curti <nico.curti2@unibo.it>
2020-07-30 18:18:08 +0200

    fall back to previous extern for autoconf installation

--- src/openslide-private.h
+++ src/openslide-private.h
@@ -336,10 +336,17 @@ void _openslide_performance_warn_once(gint *warned_flag,
 
 /* Tables */
 // YCbCr -> RGB chroma contributions
+#ifdef CMAKE_BUILD
 const int16_t _openslide_R_Cr[256];
 const int32_t _openslide_G_Cb[256];
 const int32_t _openslide_G_Cr[256];
 const int16_t _openslide_B_Cb[256];
+#else
+extern const int16_t _openslide_R_Cr[256];
+extern const int32_t _openslide_G_Cb[256];
+extern const int32_t _openslide_G_Cr[256];
+extern const int16_t _openslide_B_Cb[256];
+#endif
 
 // deprecated prefetch stuff (maybe we'll undeprecate it someday),
 // still needs these declarations for ABI compat
